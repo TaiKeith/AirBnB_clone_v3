d4c2de5fbf8afca87cd87d49d9363001a063f48b
Tai Keith <keithsteve.ks@hotmail.com>
2024-08-01 09:20:29 -0700

    Status of the API

---
+++ api/v1/views/index.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """Returns the status of the API"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def object_stats():
+    """Retrieves the number of each objects by type"""
+    stats = {
+            "amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User'),
+            }
+    return jsonify(stats)

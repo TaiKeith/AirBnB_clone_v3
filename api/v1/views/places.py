02835cd582c66794546fc7af35161ccb457dcdcd
Tai Keith <keithsteve.ks@hotmail.com>
2024-08-04 11:07:20 -0700

    Updated Place view

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,7 +5,6 @@ from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
 from models.city import City
-from models.user import User
 from models.place import Place
 
 

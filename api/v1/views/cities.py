5284aa486d515774b8579c28b54c65d4d6f6383b
wikki2000 <wisdomokposin@gmail.com>
2024-08-04 09:46:15 +0100

    add: api/v1/views/cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,7 +8,7 @@ from flask import abort, request, jsonify
 
 
 @app_views.route("/states/<state_id>/cities",
-                 methods=["GET"], strict_slashes=False)
+                 methods=["GET", "POST"], strict_slashes=False)
 def cities(state_id):
     """Handle request for cities views."""
     states = storage.all(State)
@@ -23,3 +23,56 @@ def cities(state_id):
 
         # Return the list of cities in the specified state
         return jsonify([city.to_dict() for city in state.cities])
+
+    # POST REQUEST #
+    if request.method == "POST":
+        data = request.get_json()
+        state = "State." + state_id
+        if not data:
+            abort(404, "Not a JSON")
+        elif not data.get("name"):
+            abort(400, "Missing name")
+        elif not state:
+            abort(404)
+
+        city = City(state_id=state_id, name=data.get("name"))
+        return jsonify(city.to_dict())
+
+
+@app_views.route("/cities/<city_id>", methods=["GET", "PUT", "DELETE"], strict_slashes=False)
+def city(city_id):
+    """Handle request for city views."""
+    cities = storage.all(City)
+
+    # GET REQUEST #
+    if request.method == "GET":
+        city = cities.get("City." + city_id)
+        if not city:
+            abort(404)
+        return jsonify(city.to_dict())
+
+    # PUT REQUEST #
+    if request.method == "PUT":
+        data = request.get_json()
+        city = cities.get("City." + city_id)
+        if not data:
+            abort(404, "Not a JSON")
+        elif not data.get("name"):
+            abort(400, "Missing name")
+        elif not city:
+            abort(404)
+        city.name = data.get("name")
+        storage.new(city)
+        storage.save()
+        storage.close()
+        return jsonify(city.to_dict())
+
+    # DELETE REQUEST #
+    if request.method == "DELETE":
+        city = cities.get("City." + city_id)
+        if not city:
+            abort(404)
+        storage.delete(city)
+        storage.save()
+        storage.close()
+        return jsonify({}), 200

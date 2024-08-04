78d8dc4fd37c40bd6cb510796f6ed112857ed148
Tai Keith <keithsteve.ks@hotmail.com>
2024-08-04 11:58:10 -0700

    Updated Place view

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 from models import storage
 from models.city import City
 from models.place import Place
+from models.user import User
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
@@ -19,7 +20,8 @@ def get_places(city_id):
     return jsonify([place.to_dict() for place in places])
 
 
-@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_place(place_id):
     """Retrieves a Place object based on its ID"""
     place = storage.get(Place, place_id)
@@ -65,7 +67,7 @@ def create_place(city_id):
     new_place = Place(**data)
     storage.new(new_place)
     storage.save()
-    return jsonify(new_pace.to_dict()), 201
+    return jsonify(new_place.to_dict()), 201
 
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
